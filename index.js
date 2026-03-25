c2db28eb8eb8c5ae2773069fa1eab9be53d579e4
proyectodigitalcolombia-ai <proyectodigitalcolombia@gmail.com>
2026-03-25 14:10:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -939,7 +939,7 @@ app.get('/', async (req, res) => {
    <datalist id="list_ciud">${opts.ciudades.map(c=>`<option value="${c}">`).join('')}</datalist>
    <div class="fg"><label>Oficina</label><select name="oficina">${opts.oficina.map(o=>`<option value="${o}">${o}</option>`).join('')}</select></div>
    <div class="fg"><label>Empresa Generadora</label><select name="emp_gen"><option value="TRANSPORTES SARVI">TRANSPORTES SARVI</option></select></div>
-   <div class="fg"><label>Comercial</label><select name="comercial"><option value="RAÚL LÓPEZ">RAÚL LÓPEZ</option></select></div>
+   <div class="fg"><label>Comercial</label><select name="comercial"><option value="SANDRA FORIGUA">SANDRA FORIGUA</option><option value="SLOTHOWER GARCÍA">SLOTHOWER GARCÍA</option><option value="JHON ABRIL">JHON ABRIL</option><option value="RENÉ SANDOVAL">RENÉ SANDOVAL</option></select></div>
    <div class="fg"><label>Puerto</label><select name="pto">${opts.puertos.map(o=>`<option value="${o}">${o}</option>`).join('')}</select></div>
    <div class="fg"><label>Refleja</label><select name="refleja"><option value="SI">SI</option><option value="NO">NO</option></select></div>
    <div class="fg"><label>Fecha Documento</label><input name="f_doc" type="date"></div>
